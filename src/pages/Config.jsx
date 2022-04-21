f70ea22f70aece0979cbf8f6b0c28fafda9ede4f
Guilherme R <grsguiga@gmail.com>
2022-04-21 17:14:40 -0300

    Adiciona botão voltar em configurações

--- src/pages/Config.jsx
+++ src/pages/Config.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class Config extends React.Component {
   render() {
     return (
+      <>
       <h2 data-testid="settings-title">Configurações</h2>
+      <Link to="/project-trivia">
+        <button>Voltar</button>
+      </Link>
+      </>
     );
   }
 }
